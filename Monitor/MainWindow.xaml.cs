7aee86cd980c6b6e39c1c517d2d62fb25c73af00
lixinyang <lixinyangemil@outlook.com>
2021-12-29 20:49:41 +0800

    complete chart

--- Monitor/MainWindow.xaml.cs
+++ Monitor/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using LiveCharts;
+using LiveCharts.Wpf;
+using System.Windows;
 
 namespace Monitor
 {
@@ -10,6 +12,30 @@ namespace Monitor
         public MainWindow()
         {
             InitializeComponent();
+
+            chart1.Series = new SeriesCollection()
+            {
+                new LineSeries()
+                {
+                    Values = new ChartValues<int> { 1, 3, -7, 4, 6, -2, 10, 5 }
+                }
+            };
+
+            chart2.Series = new SeriesCollection()
+            {
+                new LineSeries()
+                {
+                    Values = new ChartValues<int> { -1, 3, 7, -4, 6, -2, -10, 5 }
+                }
+            };
+
+            chart3.Series = new SeriesCollection()
+            {
+                new LineSeries()
+                {
+                    Values = new ChartValues<int> { 1, -3, -7, 4, -6, 2, 10, 5 }
+                }
+            };
         }
     }
 }
